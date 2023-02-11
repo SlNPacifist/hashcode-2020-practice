fe7fa4a367d281e1121b0e57ca89303f0262662e
Slava Baginov <slava-b@yandex-team.ru>
2023-02-11 15:56:10 +0300

    Add input.tx

--- src/input.ts
+++ src/input.ts
@@ -1,11 +1,27 @@
 import * as fs from "fs";
-import type { InputData } from "./types";
+import type { InputData, Library } from "./types";
 import { numbers } from "./parser";
 
 export default function readFile(filename: string): InputData {
     const data = fs.readFileSync(filename, 'utf-8');
     const lines = data.split('\n').reverse();
 
+    let [b, l, d] = numbers(lines);
+    const bookPrices = numbers(lines);
+    const libraries: Array<Library> = [];
+
+    while (l--) {
+        let [n, t, m] = numbers(lines);
+        libraries.push({
+            books: numbers(lines),
+            booksPerDay: m,
+            signup: t,
+        })
+    }
+
     return {
+        bookPrices,
+        libraries,
+        days: d,
     };
 }
